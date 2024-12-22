ee98ff14f7137de282b31b41d8611272dc4e7abb
Leon Laborina <growthcycle45@gmail.com>
2024-12-22 08:49:07 +0800

    pokemon front

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PokemonGradingTool from './PokemonGradingTool';
+import './App.css';
+
+
+function App() {
+    return (
+        <div className="App">
+            <PokemonGradingTool />
+        </div>
+    );
+}
+
+export default App;
