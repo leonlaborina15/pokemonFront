37f8e3e254b61160e3f7808c206ca0635f2019e4
Leon Laborina <growthcycle45@gmail.com>
2024-12-23 10:20:37 +0800

    pokemon add pages

---
+++ src/SV05_TemporalForces.tsx
@@ -0,0 +1,101 @@
+import { useState, useEffect } from "react"
+import "./App.css"
+import { SearchForm } from "./components/shared/search-form-set"
+import CardTable from "./components/shared/card-table"
+
+type Card = {
+	card_name: string
+	card_number: string
+	set_name: string
+	rarity: string
+	tcgplayer_price: string
+	psa_10_price: string
+	price_delta: string
+	profit_potential: string
+}
+
+function App() {
+	const [cardName, setCardName] = useState<string>("")
+	const [cardNumber, setCardNumber] = useState<string>("")
+	const [language, setLanguage] = useState<string>("English")
+	const [cards, setCards] = useState<Card[]>([
+
+	])
+	const [loading, setLoading] = useState<boolean>(false)
+	const [filterDelta, setFilterDelta] = useState<string>("")
+
+	useEffect(() => {
+		console.log("Cards updated:", cards)
+	}, [cards])
+
+	const handleSearch = async () => {
+		if (!cardName && !cardNumber) return
+		setLoading(true)
+
+		try {
+			const params = new URLSearchParams()
+
+			if (cardName) params.append("searchQuery", cardName.trim())
+			if (cardNumber) params.append("searchQuery", cardNumber.trim())
+			params.append("language", language)
+
+			const response = await fetch(
+				`http://127.0.0.1:8000/api/cards/scrape_and_save/?${params.toString()}`
+			)
+
+			if (!response.ok) {
+				throw new Error(`HTTP error! status: ${response.status}`)
+			}
+
+			const data = await response.json()
+
+			const filteredData: Card[] = data.filter(
+				(card: Card) => card.set_name === "SV05: Temporal Forces"
+			)
+			setCards(filteredData)
+		} catch (error) {
+			console.error("Error fetching cards:", error)
+			setCards([])
+		} finally {
+			setLoading(false)
+		}
+	}
+
+	const filteredCards = cards.filter((card) => {
+		if (filterDelta) {
+			const deltaValue = parseFloat(card.price_delta.toString() || "0")
+			if (filterDelta.startsWith(">")) {
+				return deltaValue > parseFloat(filterDelta.slice(1))
+			} else if (filterDelta.startsWith("<")) {
+				return deltaValue < parseFloat(filterDelta.slice(1))
+			}
+		}
+		return true
+	})
+
+	return (
+		<>
+			<div className="max-w-5xl mx-auto py-5">
+				<h1 className="text-5xl font-bold text-center my-12">Pokemon Grading Tool</h1>
+				<SearchForm
+					cardName={cardName}
+					setCardName={setCardName}
+					cardNumber={cardNumber}
+					setCardNumber={setCardNumber}
+					language={language}
+					setLanguage={setLanguage}
+					handleSearch={handleSearch}
+					loading={loading}
+					filterDelta={filterDelta}
+					setFilterDelta={setFilterDelta}
+				/>
+				<CardTable
+					loading={loading}
+					sortedCards={filteredCards}
+				/>
+			</div>
+		</>
+	)
+}
+
+export default App
---
+++ src/components/shared/search-form-set.tsx
@@ -0,0 +1,85 @@
+import { Input } from "../ui/input"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import { Button } from "../ui/button"
+import { LoaderIcon } from "lucide-react"
+
+type SearchFormProps = {
+    cardName: string
+    setCardName: (value: string) => void
+    cardNumber: string
+    setCardNumber: (value: string) => void
+    language: string
+    setLanguage: (value: string) => void
+    handleSearch: () => void
+    loading: boolean
+    filterDelta: string
+    setFilterDelta: (value: string) => void
+}
+
+export const SearchForm = ({
+    cardName,
+    setCardName,
+    cardNumber,
+    setCardNumber,
+    language,
+    setLanguage,
+    handleSearch,
+    loading,
+    filterDelta,
+    setFilterDelta,
+}: SearchFormProps) => {
+    return (
+        <>
+            <div className="my-4 flex gap-2 justify-between w-full">
+                <Input
+                    type="text"
+                    value={cardName}
+                    id="card-name"
+                    onChange={(e) => setCardName(e.target.value)}
+                    placeholder="Enter card name"
+                />
+                <Input
+                    type="text"
+                    value={cardNumber}
+                    id="card-number"
+                    onChange={(e) => setCardNumber(e.target.value)}
+                    placeholder="Enter card number"
+                />
+                <Select
+                    value={language}
+                    onValueChange={(value) => setLanguage(value)}
+                >
+                    <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="Language" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="English">English</SelectItem>
+                        <SelectItem value="Japanese">Japanese</SelectItem>
+                    </SelectContent>
+                </Select>
+                <Button
+                    onClick={handleSearch}
+                    className="min-w-[4.375rem]"
+                    disabled={loading}
+                >
+                    {loading ? <LoaderIcon className="animate-spin" /> : "Search"}
+                </Button>
+            </div>
+            <div className="my-4 flex gap-2 justify-between w-full">
+                <Input
+                    type="text"
+                    value={filterDelta}
+                    onChange={(e) => setFilterDelta(e.target.value)}
+                    placeholder="Filter by Price Delta (e.g., >500 or <200)"
+                    className="max-w-80"
+                />
+            </div>
+        </>
+    )
+}
