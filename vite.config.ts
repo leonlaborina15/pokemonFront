f28fb6c5de79ee19675ae5ae3fe9ae5c7494dfc2
Leon Laborina <growthcycle45@gmail.com>
2024-12-23 17:47:47 +0800

    for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,16 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    base: './', // Add this line
+    build: {     // Add these build settings
+        outDir: 'dist',
+        assetsDir: 'assets',
+        sourcemap: true
+    }
 })
