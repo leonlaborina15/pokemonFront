ab30ac7c326635b89324db918fe06a1e45c61be6
Leon Laborina <growthcycle45@gmail.com>
2025-01-02 11:48:03 +0800

    add ffiltering by rarity

--- src/components/shared/search-form.tsx
+++ src/components/shared/search-form.tsx
@@ -1,126 +1,143 @@
-import { Input } from "../ui/input"
+import React from "react";
+import { Input } from "../ui/input";
 import {
-	Select,
-	SelectContent,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
-} from "@/components/ui/select"
-import { Button } from "../ui/button"
-import { Info, LoaderIcon } from "lucide-react"
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Button } from "../ui/button";
+import { Info, LoaderIcon } from "lucide-react";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 
 type SearchFormProps = {
-	cardName: string
-	setCardName: (value: string) => void
-	cardNumber: string
-	setCardNumber: (value: string) => void
-	set: string
-	setSet: (value: string) => void
-	language: string
-	setLanguage: (value: string) => void
-	handleSearch: () => void
-	loading: boolean
-	filterDelta: string
-	setFilterDelta: (value: string) => void
-}
+  cardName: string;
+  setCardName: (value: string) => void;
+  cardNumber: string;
+  setCardNumber: (value: string) => void;
+  set: string;
+  setSet: (value: string) => void;
+  language: string;
+  setLanguage: (value: string) => void;
+  handleSearch: () => void;
+  loading: boolean;
+  filterDelta: string;
+  setFilterDelta: (value: string) => void;
+  rarity: string;
+  setRarity: (value: string) => void;
+  rarityOptions: { value: string; label: string }[];
+};
 
-const SearchForm = ({
-	cardName,
-	setCardName,
-	cardNumber,
-	setCardNumber,
-	set,
-	setSet,
-	language,
-	setLanguage,
-	handleSearch,
-	loading,
-	filterDelta,
-	setFilterDelta,
-}: SearchFormProps) => {
-	return (
-		<div className="p-4">
-			<div className="max-w-xl mx-auto w-full">
-				<div className="mt-6 grid grid-cols-2 gap-2 w-full">
-					{/* Input for card name */}
-					<Input
-						type="text"
-						value={cardName}
-						id="card-name"
-						onChange={(e) => setCardName(e.target.value)}
-						placeholder="Enter card name"
-					/>
-					{/* Input for card number */}
-					<Input
-						type="text"
-						value={cardNumber}
-						id="card-number"
-						onChange={(e) => setCardNumber(e.target.value)}
-						placeholder="Enter card number"
-					/>
-					{/* Input for card set */}
-					<Input
-						type="text"
-						value={set}
-						id="card-set"
-						onChange={(e) => setSet(e.target.value)}
-						placeholder="Enter card set"
-					/>
-					{/* Input for filtering by price delta */}
-					<div className="space-y-2">
-						<div className="relative h-9">
-							<Input
-								id="filter-delta"
-								type="text"
-								value={filterDelta}
-								onChange={(e) => setFilterDelta(e.target.value)}
-								placeholder="Filter by Price Delta"
-								className="max-w-80"
-							/>
-							<TooltipProvider>
-								<Tooltip>
-									<TooltipTrigger className="absolute inset-y-0 end-0 flex w-9 items-center justify-center rounded-e-lg text-muted-foreground/80 outline-offset-2 transition-colors hover:text-foreground focus:z-10 focus-visible:outline focus-visible:outline-2 focus-visible:outline-ring/70 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50">
-										<Info
-											size={16}
-											strokeWidth={2}
-											aria-hidden="true"
-										/>
-									</TooltipTrigger>
-									<TooltipContent>
-										<p>(e.g., &lt;500 or &gt;200)</p>
-									</TooltipContent>
-								</Tooltip>
-							</TooltipProvider>
-						</div>
-					</div>
-				</div>
-				<div className="flex gap-2 items-center mt-2">
-					{/* Select for language */}
-					<Select
-						value={language}
-						onValueChange={(value) => setLanguage(value)}
-					>
-						<SelectTrigger className="w-[180px]">
-							<SelectValue placeholder="Language" />
-						</SelectTrigger>
-						<SelectContent>
-							<SelectItem value="English">English</SelectItem>
-							<SelectItem value="Japanese">Japanese</SelectItem>
-						</SelectContent>
-					</Select>
-					{/* Search button */}
-					<Button
-						onClick={handleSearch}
-						className="min-w-[4.375rem] w-full"
-						disabled={loading}
-					>
-						{loading ? <LoaderIcon className="animate-spin" /> : "Search"}
-					</Button>
-				</div>
-			</div>
-		</div>
-	)
-}
+const SearchForm: React.FC<SearchFormProps> = ({
+  cardName,
+  setCardName,
+  cardNumber,
+  setCardNumber,
+  set,
+  setSet,
+  language,
+  setLanguage,
+  handleSearch,
+  loading,
+  filterDelta,
+  setFilterDelta,
+  rarity,
+  setRarity,
+  rarityOptions,
+}) => {
+  return (
+    <div className="p-4">
+      <div className="max-w-xl mx-auto w-full">
+        <div className="mt-6 grid grid-cols-2 gap-2 w-full">
+          {/* Input for card name */}
+          <Input
+            type="text"
+            value={cardName}
+            id="card-name"
+            onChange={(e) => setCardName(e.target.value)}
+            placeholder="Enter card name"
+          />
+          {/* Input for card number */}
+          <Input
+            type="text"
+            value={cardNumber}
+            id="card-number"
+            onChange={(e) => setCardNumber(e.target.value)}
+            placeholder="Enter card number"
+          />
+          {/* Input for card set */}
+          <Input
+            type="text"
+            value={set}
+            id="card-set"
+            onChange={(e) => setSet(e.target.value)}
+            placeholder="Enter card set"
+          />
+					 {/* Input for filtering by price delta */}
+  <div className="space-y-2">
+    <div className="relative h-9">
+      <Input
+        id="filter-delta"
+        type="text"
+        value={filterDelta}
+        onChange={(e) => setFilterDelta(e.target.value)}
+        placeholder="Filter by Price Delta"
+        className="max-w-80"
+      />
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger className="absolute inset-y-0 end-0 flex w-9 items-center justify-center rounded-e-lg text-muted-foreground/80 outline-offset-2 transition-colors hover:text-foreground focus:z-10 focus-visible:outline focus-visible:outline-2 focus-visible:outline-ring/70 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50">
+            <Info size={16} strokeWidth={2} aria-hidden="true" />
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>(e.g., &lt;500 or &gt;200)</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
+  </div>
 
-export default SearchForm
+        </div>
+
+        <div className="flex gap-2 items-center mt-2">
+          {/* Select for language */}
+          <Select value={language} onValueChange={(value) => setLanguage(value)}>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Language" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="English">English</SelectItem>
+              <SelectItem value="Japanese">Japanese</SelectItem>
+            </SelectContent>
+          </Select>
+
+					 {/* Select for rarity */}
+          <Select value={rarity} onValueChange={(value) => setRarity(value)}>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Select Rarity" />
+            </SelectTrigger>
+            <SelectContent>
+              {rarityOptions.map((option) => (
+                <SelectItem key={option.value} value={option.value}>
+                  {option.label}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+					
+          {/* Search button */}
+          <Button
+            onClick={handleSearch}
+            className="min-w-[4.375rem] w-full"
+            disabled={loading}
+          >
+            {loading ? <LoaderIcon className="animate-spin" /> : "Search"}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchForm;
--- src/pages/all-cards/index.tsx
+++ src/pages/all-cards/index.tsx
@@ -1,105 +1,130 @@
-import { useState, useEffect } from "react"
-import React from "react"
-import { CardTable, SearchForm } from "@/components/shared"
-import { motion } from "motion/react"
+import { useState, useEffect } from "react";
+import React from "react";
+import { CardTable, SearchForm } from "@/components/shared";
+import { motion } from "motion/react";
 
 type Card = {
-	card_name: string
-	card_number: string
-	set_name: string
-	rarity: string
-	tcgplayer_price: string
-	psa_10_price: string
-	price_delta: string
-	profit_potential: string
-}
+  card_name: string;
+  card_number: string;
+  set_name: string;
+  rarity: string;
+  tcgplayer_price: string;
+  psa_10_price: string;
+  price_delta: string;
+  profit_potential: string;
+};
+
+const rarityOptionsEnglish = [
+	  { value: "All", label: "All Rarity" },
+  { value: "Illustration Rare", label: "Illustration Rare" },
+  { value: "Special Illustration Rare", label: "Special Illustration Rare" },
+  { value: "Hyper Rare", label: "Hyper Rare" },
+  { value: "Ultra Rare", label: "Ultra Rare" },
+];
+
+const rarityOptionsJapanese = [
+		  { value: "All", label: "All Rarity" },
+  { value: "Special Art Rare", label: "Special Art Rare" },
+  { value: "Super Rare", label: "Super Rare" },
+  { value: "Ultra Rare", label: "Ultra Rare" },
+	  { value: "Art Rare", label: "Art Rare" },
+
+];
 
 const AllCards: React.FC = () => {
-	const [cardName, setCardName] = useState<string>("")
-	const [cardNumber, setCardNumber] = useState<string>("")
-	const [set, setSet] = useState<string>("")
-	const [language, setLanguage] = useState<string>("English")
-	const [cards, setCards] = useState<Card[]>([])
-	const [loading, setLoading] = useState<boolean>(false)
-	const [filterDelta, setFilterDelta] = useState<string>("")
+  const [cardName, setCardName] = useState<string>("");
+  const [cardNumber, setCardNumber] = useState<string>("");
+  const [set, setSet] = useState<string>("");
+  const [language, setLanguage] = useState<string>("English");
+  const [cards, setCards] = useState<Card[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [filterDelta, setFilterDelta] = useState<string>("");
+  const [rarity, setRarity] = useState<string>("");
 
-	useEffect(() => {
-		console.log("Cards updated:", cards)
-	}, [cards])
+  useEffect(() => {
+    console.log("Cards updated:", cards);
+  }, [cards]);
 
-	const handleSearch = async () => {
-		if (!cardName && !cardNumber) return
-		setLoading(true)
+  const handleSearch = async () => {
+    if (!cardName && !cardNumber) return;
+    setLoading(true);
 
-		try {
-			const params = new URLSearchParams()
+    try {
+      const params = new URLSearchParams();
 
-			if (cardName) params.append("searchQuery", cardName.trim())
-			if (cardNumber) params.append("searchQuery", cardNumber.trim())
-			params.append("language", language)
+      if (cardName) params.append("searchQuery", cardName.trim());
+      if (cardNumber) params.append("searchQuery", cardNumber.trim());
+      params.append("language", language);
 
-			const response = await fetch(
-				`https://pokemongradingtool-production.up.railway.app/api/cards/scrape_and_save/?${params.toString()}`
-			)
+      const response = await fetch(
+        `https://pokemongradingtool-production.up.railway.app/api/cards/scrape_and_save/?${params.toString()}`
+      );
 
-			if (!response.ok) {
-				throw new Error(`HTTP error! status: ${response.status}`)
-			}
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
 
-			const data = await response.json()
+      const data = await response.json();
+      const filteredData: Card[] = set
+        ? data.filter((card: Card) => card.set_name === set)
+        : data;
 
-			const filteredData: Card[] = set ? data.filter((card: Card) => card.set_name === set) : data
-			setCards(filteredData)
-		} catch (error) {
-			console.error("Error fetching cards:", error)
-			setCards([])
-		} finally {
-			setLoading(false)
-		}
-	}
+      setCards(filteredData);
+    } catch (error) {
+      console.error("Error fetching cards:", error);
+      setCards([]);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-	const filteredCards = cards.filter((card) => {
-		if (filterDelta) {
-			const deltaValue = parseFloat(card.price_delta.toString() || "0")
-			if (filterDelta.startsWith(">")) {
-				return deltaValue > parseFloat(filterDelta.slice(1))
-			} else if (filterDelta.startsWith("<")) {
-				return deltaValue < parseFloat(filterDelta.slice(1))
-			}
-		}
-		return true
-	})
-	return (
-		<motion.section
-			initial={{ opacity: 0 }}
-			animate={{ opacity: 1 }}
-			exit={{ opacity: 0 }}
-			transition={{ duration: 0.5, ease: "easeInOut" }}
-			aria-label="AllCards Section"
-			className="min-h-[calc(100vh-64px)]"
-		>
-			{" "}
-			<h1 className="text-center">Pokemon Grading Tool</h1>
-			<SearchForm
-				cardName={cardName}
-				setCardName={setCardName}
-				cardNumber={cardNumber}
-				setCardNumber={setCardNumber}
-				set={set}
-				setSet={setSet}
-				language={language}
-				setLanguage={setLanguage}
-				handleSearch={handleSearch}
-				loading={loading}
-				filterDelta={filterDelta}
-				setFilterDelta={setFilterDelta}
-			/>
-			<CardTable
-				loading={loading}
-				sortedCards={filteredCards}
-			/>
-		</motion.section>
-	)
-}
+const filteredCards = cards.filter((card) => {
+    if (filterDelta) {
+      const deltaValue = parseFloat(card.price_delta || "0");
+      if (filterDelta.startsWith(">")) {
+        return deltaValue > parseFloat(filterDelta.slice(1));
+      } else if (filterDelta.startsWith("<")) {
+        return deltaValue < parseFloat(filterDelta.slice(1));
+      }
+    }
+    if (rarity && rarity !== "All") {
+      return card.rarity === rarity;
+    }
+    return true;
+  });
 
-export default AllCards
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5, ease: "easeInOut" }}
+      aria-label="AllCards Section"
+      className="min-h-[calc(100vh-64px)]"
+    >
+      <h1 className="text-center">Pokemon Grading Tool</h1>
+      <SearchForm
+        cardName={cardName}
+        setCardName={setCardName}
+        cardNumber={cardNumber}
+        setCardNumber={setCardNumber}
+        set={set}
+        setSet={setSet}
+        language={language}
+        setLanguage={setLanguage}
+        handleSearch={handleSearch}
+        loading={loading}
+        filterDelta={filterDelta}
+        setFilterDelta={setFilterDelta}
+        rarity={rarity}
+        setRarity={setRarity}
+        rarityOptions={
+          language === "English" ? rarityOptionsEnglish : rarityOptionsJapanese
+        }
+      />
+      <CardTable loading={loading} sortedCards={filteredCards} />
+    </motion.section>
+  );
+};
+
+export default AllCards;
